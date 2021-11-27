646a44e989350477e5ae78b0b328a88599122bb9
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2021-11-28 01:03:16 +0500

    firebase auth signup

--- lib/register.dart
+++ lib/register.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Register extends StatelessWidget {
   const Register({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController usernameController = TextEditingController();
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
+    void register() async {
+      FirebaseAuth auth = FirebaseAuth.instance;
+      final String username = usernameController.text;
+      final String email = emailController.text;
+      final String password = passwordController.text;
+
+      try {
+        await auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      } catch (e) {
+        print("errrro"+e.toString());
+      }
+    }
+
     return Scaffold(
       body: Center(
         child: Container(
@@ -13,21 +32,24 @@ class Register extends StatelessWidget {
             child: Column(
               children: [
                 TextFormField(
+                  controller: usernameController,
                   decoration: InputDecoration(
                     labelText: 'Enter your username',
                   ),
                 ),
                 TextFormField(
+                  controller: emailController,
                   decoration: InputDecoration(
                     labelText: 'Enter your email',
                   ),
                 ),
                 TextFormField(
+                  controller: passwordController,
                   decoration: InputDecoration(
                     labelText: 'Enter your password',
                   ),
                 ),
-                ElevatedButton(onPressed: () {}, child: Text("Register"))
+                ElevatedButton(onPressed: register, child: Text("Register"))
               ],
             ),
           ),
